a1e57bd8a7f4c815f76bce83ac6247ad2fa2d729
ZijieXiong <zx811@nyu.edu>
2023-12-13 02:56:36 -0500

    added more visualization

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
 var song;
 var vocal;
 var guitar;
-
+var bass;
+var drum;
 
 var fft;
 var fft_vocal;
 var fft_guitar;
+var fft_bass;
+var fft_drum;
 
 var amp_guitar;
 
 var particles = [];
 var sun;
 var bridge;
+var clouds = [];
+var tomori;
 
 var s;
 
@@ -20,16 +25,32 @@ var yx = [];
 
 var dev = false;
 var freq = 'bass';
+var isShaking = false;
 
 function probability(p){
   return p>random(100)
 }
 
+function generateRandomValue() {
+  // Generate a random number between 0 and 1
+  const randomNumber = Math.random();
+
+  // Map the random number to 1 or -1 with a 50% chance each
+  if (randomNumber < 0.5) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
 function preload() {
   print("loading");
   song = loadSound("mygo/mygo.flac");
-  vocal = loadSound("compressed/vocals.mp3");
+  vocal = loadSound("mygo/vocals.wav");
   guitar = loadSound("compressed/other.mp3");
+  bass = loadSound("mygo/bass.wav");
+  drum = loadSound("mygo/drums.wav")
+
 
   //img = loadImage("bg.jpg");
 }
@@ -44,20 +65,42 @@ function setup() {
   frameRate(60);
   vocal.setVolume(0.1);
   guitar.setVolume(0.1);
+  bass.setVolume(.1);
+  drum.setVolume(.1);
   fft_vocal = new p5.FFT();
   fft_vocal.setInput(vocal);
+  fft_bass = new p5.FFT();
+  fft_bass.setInput(bass);
+  fft_drum = new p5.FFT();
+  fft_drum.setInput(drum);
 
   //amplifier = new p5.Amplitude();
   //amplifier.setInput(song);
   amp_guitar = new p5.Amplitude();
   amp_guitar.setInput(guitar);
 
+
   s = new Scribble();
   //img.filter(BLUR, 12);
   //noLoop();
   sun = new Sun(-100,height/2);
   bridge = new Bridge();
-  noSmooth();
+
+  overlayCanvas = createGraphics(width,height);
+  overlayCanvas.clear();
+  overlayCanvas.noFill();
+  overlayCanvas.stroke(255,50);
+
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let cloudSize = random(30, 80);
+    let y = random(0+cloudSize,height/4);
+    let cloud = new Cloud(x, y, cloudSize);
+    clouds.push(cloud);
+  }
+
+  tomori = new Tomori(width/2, height/2, 0.5);
+  //noSmooth();
 }
 
 function draw() {
@@ -91,6 +134,22 @@ function draw() {
     trebleButton.position(250,10);
     trebleButton.mousePressed(switchTreble);
   }
+    fft_drum.analyze();
+    let level = fft_drum.getEnergy('lowMid');
+    print(level);
+    if (level > 100 && !isShaking) {
+      isShaking = true;
+      startShake();
+    }
+  
+    if (isShaking) {
+      let shakeAmplitude = map(level, 100, 150, 0, 2);
+      let dx = random(-shakeAmplitude, shakeAmplitude);
+      let dy = random(-shakeAmplitude, shakeAmplitude);
+      translate(dx, dy);
+    }
+  
+
 
   let temp = map(song.currentTime(),0,song.duration(),0,1);
   let bgcolor = lerpColor(color(135,206,250),  color(240, 200, 150), temp);
@@ -102,17 +161,36 @@ function draw() {
 
   sun.x = map(progress, 0, 1, 0 - sun.radius - 30, width + sun.radius + 30);
   if(progress >= song.duration()/2){
-    sun.y = map(progress, 0.5, 0, height/7, height/6);
+    sun.y = map(progress, 0.5, 0, height/8, height/7);
   }
   else{
-    sun.y = map(progress, 0, 0.5, height/6, height/7);
+    sun.y = map(progress, 0, 0.5, height/7, height/8);
   }
 
   sun.display();
+
+  for (var i = 0; i < clouds.length; i++) {
+    clouds[i].display();
+    if(song.isPlaying()){
+      clouds[i].move(1);
+    }
+    if(clouds[i].edges()){
+      if(clouds[i].dir > 0){
+        clouds[i].x = -clouds[i].size;
+      }
+      else{
+        clouds[i].x = width + clouds[i].size
+      }
+
+      clouds[i].y = random(0+clouds[i].size,height/4);
+    }
+  }
+
+  //tomori.display();
   bridge.display();
 
-  for(var i =0; i< wave.length;i+=200){
-    var y = map(wave[i], -0.5,0.5,height/5*3, height);
+  for(var i =0; i< wave.length;i+=5){
+    var y = map(wave[i], -0.3,0.3,height/5*3, height);
     var x = map(i, 0, wave.length-1, 0, width);
     vertex(x,y);
     xs.push(x);
@@ -133,7 +211,7 @@ function draw() {
   for (var i = 0; i < particles.length; i++) {
     if(!particles[i].edges()){
       //particles[i].vx = map(amp, 0.5, 0.25, 0, particles[i].vXMax)*Math.sign(particles[i].vX);
-      particles[i].vy = map(amp, 0, 0.22, 0, particles[i].vYMax);
+      particles[i].vy = map(amp, 0, 0.22, 0.1, particles[i].vYMax);
       particles[i].display(); 
       particles[i].move();
 
@@ -142,6 +220,23 @@ function draw() {
       delete temp[0];
     }
   }
+
+  fft_bass.analyze();
+  let bassAmp = fft_bass.getEnergy('bass');
+  if(bassAmp < 100){
+    var alpha = map(bassAmp, 100, 0, 30, 50);
+  }
+  else if(bassAmp > 190){
+    var alpha = 0
+  }
+  else{
+    var alpha = map(bassAmp, 180, 120, 10, 30);
+  }
+  overlayCanvas.clear();
+  overlayCanvas.fill(0,0,0,alpha);
+  overlayCanvas.rect(0,0,width,height);
+
+  image(overlayCanvas, width/2, height/2);
 }
 
 function mousePressed(){
@@ -159,10 +254,14 @@ function playMusic() {
     song.pause();
     vocal.pause();
     guitar.pause();
+    bass.pause();
+    drum.pause();
   } else {
     song.play();
     vocal.play();
     guitar.play();
+    bass.play();
+    drum.play();
   }
 }
 
@@ -193,6 +292,12 @@ function updateProgress() {
   guitar.jump(value * song.duration());
 }
 
+function startShake() {
+  setTimeout(() => {
+    isShaking = false;
+    translate(0, 0);
+  }, 100);
+}
 
 class CherryBlossomPetal {
   constructor(x, y) {
@@ -202,10 +307,11 @@ class CherryBlossomPetal {
     this.xtrans = 0;
     this.vXMax = random(2);
     this.vx = this.vXMax;
-    this.vYMax = random(5,10);
+    this.vYMax = random(10);
     this.vy = this.vYMax;
     this.size = random(10, 20);
     this.color = color(random(200, 255), random(150, 200), random(200, 255));
+    this.rotate = random(-20,20);
   }
 
   display() {
@@ -213,7 +319,7 @@ class CherryBlossomPetal {
     fill(this.color);
     push();
     translate(this.x,this.y);
-    rotate(20)
+    rotate(this.rotate);
     beginShape();
     curveVertex(this.size / 2, 0);
     curveVertex(this.size / 2, 0);
@@ -259,6 +365,7 @@ class Sun {
   }
 
   display() {
+    
     this.xs = [];
     this.ys = [];
     for(var i = 0; i < 360; i++){
@@ -271,12 +378,11 @@ class Sun {
     stroke(255, 255, 0);
     strokeWeight(3);
     s.scribbleFilling(this.xs,this.ys,10, 10);
-    stroke(255, 255, 0);
+    stroke(255, 215, 0);
     strokeWeight(3);
-    //fill(255,255,0);
+    //fill(255,215,0);
     s.scribbleEllipse(this.x,this.y,this.radius*2,this.radius*2);
     //ellipse(this.x,this.y,this.radius*2);
-    stroke(255, 255, 0);
     strokeWeight(2);
     let angleIncrement = 360 / this.numRays;
     if(song.isPlaying()){
@@ -305,28 +411,29 @@ class Bridge{
     this.pileY = (height/5*3 + height)/2 - this.pileHeight/2;
     this.bodyHeight = 50
     this.bodyY = (height/5*3 + height)/2 - this.pileHeight/4*3 - this.bodyHeight/2;
-    this.bodyXs = [0,width,0,width];
-    this.bodyYs = [this.bodyY-this.bodyHeight/2,this.bodyY-this.bodyHeight/2,this.bodyY+this.bodyHeight/2,this.bodyY+this.bodyHeight/2]
+    this.bodyXs = [0,0,width,width];
+    this.bodyYs = [this.bodyY-this.bodyHeight/2,this.bodyY+this.bodyHeight/2,this.bodyY+this.bodyHeight/2,this.bodyY-this.bodyHeight/2]
     this.fenceHeight = 40;
     this.fenceY = this.bodyY-this.bodyHeight/2-this.fenceHeight/2;
   }
 
   display(){
     stroke(192, 192, 192);
+    noFill();
     for(var i = 0; i < this.numOfPile; i++){
       let x = map(i,0,this.numOfPile, width/5, width);
       let y = this.pileY;
-      s.scribbleRect(x,y,this.pileWidth,this.pileHeight);
-      //rect(x,y,this.pileWidth,this.pileHeight);
-      let xs = [x - this.pileWidth/2, x + this.pileWidth/2, x - this.pileWidth/2, x + this.pileWidth/2];
+      //s.scribbleRect(x,y,this.pileWidth,this.pileHeight);
+      rect(x,y,this.pileWidth,this.pileHeight);
+      let xs = [x - this.pileWidth/2, x + this.pileWidth/2, x + this.pileWidth/2, x - this.pileWidth/2];
       let ys = [y + this.pileHeight/2, y + this.pileHeight/2, y - this.pileHeight/2, y - this.pileHeight/2];
-      s.scribbleFilling(xs,ys,10,10);
+      s.scribbleFilling(xs,ys,5,10);
     }
 
     stroke(169);
     rect(this.x,this.bodyY,width,this.bodyHeight);
     //s.scribbleRect(this.x,this.bodyY,width,this.bodyHeight);
-    s.scribbleFilling(this.bodyXs, this.bodyYs, 10,50);
+    s.scribbleFilling(this.bodyXs, this.bodyYs, 5,30);
 
     stroke(128,128,128);
     for(var i = 0; i < this.numOfFence; i++){
@@ -339,4 +446,77 @@ class Bridge{
 
   }
 
+}
+
+class Cloud {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.dir = generateRandomValue();
+  }
+
+  display() {
+    noStroke();
+    fill(255);
+    s.scribbleEllipse(this.x, this.y, this.size, this.size);
+    s.scribbleEllipse(this.x + this.size * 0.5, this.y - this.size * 0.2, this.size * 0.8, this.size * 0.8);
+    s.scribbleEllipse(this.x - this.size * 0.3, this.y - this.size * 0.2, this.size * 0.7, this.size * 0.7);
+    s.scribbleEllipse(this.x + this.size * 0.3, this.y + this.size * 0.2, this.size * 0.7, this.size * 0.7);
+    s.scribbleEllipse(this.x - this.size * 0.2, this.y + this.size * 0.2, this.size * 0.6, this.size * 0.6);
+  }
+
+  move(speed) {
+    this.x += speed * this.dir;
+
+  }
+
+  edges(){
+    if (this.x > width + this.size || this.x<-this.size) {
+      print("out");
+      return true;
+    }
+    return false;
+  }
+}
+
+class Tomori {
+  constructor(x, y, scale) {
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+  }
+
+  display() {
+    let headSize = 40 * this.scale;
+    let bodyLength = 60 * this.scale;
+    let legLength = 60 * this.scale;
+    let armLength = 40 * this.scale;
+
+
+
+
+    //head
+    fill(255, 228, 196);
+    ellipse(this.x, this.y - headSize * 0.5, headSize, headSize);
+    //hair
+    fill(128, 0, 128); // 紫色
+    arc(this.x, this.y - headSize * 0.5, headSize, headSize, 180, 360);
+    // body
+    fill(0, 128, 0);
+    stroke(0);
+    line(this.x, this.y + headSize * 0.5, this.x, this.y + headSize * 0.5 + bodyLength);
+
+    // left leg
+    line(this.x, this.y + headSize * 0.5 + bodyLength, this.x - legLength * 0.3, this.y + headSize * 0.5 + bodyLength + legLength);
+
+    // right leg
+    line(this.x, this.y + headSize * 0.5 + bodyLength, this.x + legLength * 0.3, this.y + headSize * 0.5 + bodyLength + legLength);
+
+    // left arm
+    line(this.x, this.y + headSize * 0.5 + 10, this.x - armLength * 0.3, this.y + headSize * 0.5 + 10 + armLength);
+
+    // right arm
+    line(this.x, this.y + headSize * 0.5 + 10, this.x + armLength * 0.3, this.y + headSize * 0.5 + 10 + armLength);
+  }
 }
